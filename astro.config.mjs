1adc0e65e182160367e4d22cff41023b9c30a864
Barrier <nudnouil@gmail.com>
2025-03-25 07:05:01 -0500

    finishing up, adding icons, and about me page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,3 +12,4 @@ export default defineConfig({
 		port: 3000,
 	  },
 });
+
